783c39eb65d6c3a64932b166b898e7aa937e0654
Steve Marsh <Zimdale@gmail.com>
2014-02-02 18:00:48 -0500

    Clean that up a bit

--- dir.lisp
+++ dir.lisp
@@ -1,11 +1,9 @@
 
 ;Start out by reading a file. sure why not.
 (let ((in (open "dir.lisp")))
-	(when in
 		(loop for line = (read-line in nil)
 			while line do (print line)
 		)
-	)
 	(close in)
 
 )
