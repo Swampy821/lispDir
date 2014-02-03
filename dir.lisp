181b999903b6b38f36d933f8be87107d2caddcc8
Steve Marsh <Zimdale@gmail.com>
2014-02-02 21:21:06 -0500

    Added more to the file Still needs a lot of work

--- dir.lisp
+++ dir.lisp
@@ -7,3 +7,17 @@
 	(close in)
 
 )
+;Now lets write to a file called MAP.txt
+;Get the basics out of the way and then move to looping through a directory
+(setf fileName "MAP.txt")")"
+(setf content "FILE CONTENT")
+
+(with-open-file (stream fileName :external-format charset:iso-8859-1
+								   :direction :output
+								   :if-exists :overwrite
+								   :if-does-not-exist :create)
+	(format stream content)
+)
+
+(print (concatenate 'string "File " fileName " " "written successfully!"))
+
